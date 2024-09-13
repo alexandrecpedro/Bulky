e99f2e71e596e57106df45b7ba85f1fd09cbc8dc
Alexandre Pedro <alexandrecpedro@gmail.com>
2024-09-13 13:38:42 -0300

    feat: create category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,24 +1,35 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Controllers;
 
 public class CategoryController : Controller
 {
     private readonly ApplicationDbContext _db;
-    public CategoryController(ApplicationDbContext db)
+    private readonly ILogger<CategoryController> _logger;
+    public CategoryController(ApplicationDbContext db, ILogger<CategoryController> logger)
     {
         _db = db;
+        _logger = logger;
     }
-    public IActionResult Index()
+    public async Task<IActionResult> Index(int page = 1, int pageSize = 10)
     {
-        List<Category> objCategoryList = [.. _db.Categories];
+        List<Category> objCategoryList = await _db.Categories
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .AsNoTracking()
+            .ToListAsync();
+
         return View(objCategoryList);
     }
 
-    public IActionResult Create()
+    [HttpPost]
+    public async Task<IActionResult> Create(Category category)
     {
-        return View();
+        await _db.Categories.AddAsync(category);
+        await _db.SaveChangesAsync();
+        return RedirectToAction("Index");
     }
 }
