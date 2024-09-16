ead2ee11c480641451f36bffeeffe0009c0ccc14
Alexandre Pedro <alexandre.pedro@itlean.com.br>
2024-09-16 14:27:28 -0300

    feat: get category details to edit

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -50,4 +50,41 @@ public class CategoryController : Controller
 
         return View();
     }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Category? categoryFromDb = await _db.Categories.FindAsync(id);
+        //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+        //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Edit(Category category)
+    {
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name!");
+        }
+        //if (category.Name is not null && category.Name.Equals("test", StringComparison.CurrentCultureIgnoreCase))
+        //{
+        //    ModelState.AddModelError("", "Test is an invalid value!");
+        //}
+        if (ModelState.IsValid)
+        {
+            await _db.Categories.AddAsync(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
 }
