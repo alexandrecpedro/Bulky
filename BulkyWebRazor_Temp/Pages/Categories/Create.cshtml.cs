67474dbe7036efbf0dbc254059ca8fb1b67ac7b4
Alexandre Pedro <alexandre.pedro@itlean.com.br>
2024-09-16 17:29:54 -0300

    styles: create category ui

--- BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,22 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BulkyWebRazor_Temp.Pages.Categories
+namespace BulkyWebRazor_Temp.Pages.Categories;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly ApplicationDbContext _db;
+    private readonly ILogger<CreateModel> _logger;
+    public Category Category { get; set; }
+    public CreateModel(ApplicationDbContext db, ILogger<CreateModel> logger)
     {
-        public void OnGet()
-        {
-        }
+        _db = db;
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
     }
 }
