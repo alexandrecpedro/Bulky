3d71a5659a79d8cecaeb09db746fdf1757eb7b6a
Alexandre Pedro <alexandre.pedro@itlean.com.br>
2024-09-17 18:29:31 -0300

    feat: areas in .NET

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -30,7 +30,7 @@ app.UseSession();
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
