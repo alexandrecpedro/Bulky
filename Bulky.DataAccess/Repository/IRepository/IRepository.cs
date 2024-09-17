e3e7717cbe73d9f2e58eb660aeee09bf2aa6ea01
Alexandre Pedro <alexandre.pedro@itlean.com.br>
2024-09-17 15:54:38 -0300

    feat: generic repository interface

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository.IRepository;
+
+internal interface IRepository<T> where T : class
+{
+    IQueryable<T> GetAll(int? page, int? pageSize);
+    T Get(Expression<Func<T, bool>> filter);
+    void Add(T entity);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entity);
+}
