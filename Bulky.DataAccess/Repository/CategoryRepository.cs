40a83f0470eb13f25fbb2ff9b09a901d6b268a1b
Alexandre Pedro <alexandre.pedro@itlean.com.br>
2024-09-17 16:58:46 -0300

    feat: implement category interface and repository

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public async Task Save()
+    {
+        await _db.SaveChangesAsync();
+    }
+
+    public async Task Update(Category category)
+    {
+        _db.Categories.Update(category);
+    }
+}
---
+++ Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository.IRepository;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    Task Update(Category category);
+    Task Save();
+}
