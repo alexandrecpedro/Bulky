5207ce6d8933a55949b8f1b96e3e216fb14dea77
Alexandre Pedro <alexandre.pedro@itlean.com.br>
2024-09-21 13:21:07 -0300

    refactor(product): combine create and edit pages

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -22,7 +22,7 @@ public class ProductController : Controller
         return View(objProductList);
     }
 
-    public IActionResult Create()
+    public async Task<IActionResult> Upsert(int? id)
     {
         ProductVM productVM = new()
         {
@@ -34,27 +34,52 @@ public class ProductController : Controller
                 }),
             Product = new Product()
         };
-        //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
-        //    .GetAll().Select(u => new SelectListItem
-        //    {
-        //        Text = u.Name,
-        //        Value = u.Id.ToString(),
-        //    });
 
-        //ViewBag.CategoryList = CategoryList;
-        //ViewData["CategoryList"] = CategoryList;
+        // CREATE
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
 
+        // UPDATE
+        Product? productFromDb = await _unitOfWork.Product.Get(u => u.Id == id);
+        //Product? productFromDb1 = _db.Products.FirstOrDefault(u=>u.Id==id);
+        //Product? productFromDb2 = _db.Products.Where(u=>u.Id==id).FirstOrDefault();
+
+        if (productFromDb == null)
+        {
+            return NotFound();
+        }
+
+        productVM.Product = productFromDb;
         return View(productVM);
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create(ProductVM productVM)
+    public async Task<IActionResult> Upsert(ProductVM productVM, IFormFile? file)
     {
         if (ModelState.IsValid)
         {
-            await _unitOfWork.Product.Add(productVM.Product);
+            string successMessage;
+
+            if (productVM.Product.Id == 0)
+            {
+                await _unitOfWork.Product.Add(productVM.Product);
+                successMessage = "Product created successfully!";
+            }
+            else
+            {
+                _unitOfWork.Product.Update(productVM.Product);
+                successMessage = "Product updated successfully!";
+            }
+
+            //if (file != null && file.Length > 0)
+            //{
+
+            //}
+            
             await _unitOfWork.Save();
-            TempData["success"] = "Product created successfully!";
+            TempData["success"] = successMessage;
             return RedirectToAction("Index");
         }
 
@@ -66,37 +91,6 @@ public class ProductController : Controller
                 });
 
         return View(productVM);
-    }
-
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null || id == 0)
-        {
-            return NotFound();
-        }
-        Product? productFromDb = await _unitOfWork.Product.Get(u => u.Id == id);
-        //Product? productFromDb1 = _db.Products.FirstOrDefault(u=>u.Id==id);
-        //Product? productFromDb2 = _db.Products.Where(u=>u.Id==id).FirstOrDefault();
-        
-        if (productFromDb == null)
-        {
-            return NotFound();
-        }
-        return View(productFromDb);
-    }
-
-    [HttpPost]
-    public async Task<IActionResult> Edit(Product product)
-    {
-        if (ModelState.IsValid)
-        {
-            _unitOfWork.Product.Update(product);
-            await _unitOfWork.Save();
-            TempData["success"] = "Product updated successfully!";
-            return RedirectToAction("Index");
-        }
-
-        return View();
     }
 
     public async Task<IActionResult> Delete(int? id)
